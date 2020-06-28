4bf575210f116be79cd9c1eea41d75deff47d216
Steven Dana <stevendana86@gmail.com>
2020-06-27 20:42:06 -0400

    added inter google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,15 @@ module.exports = {
         cookieDomain: "www.stevendana.com",
       }
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `inter\:300,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
