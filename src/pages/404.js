5946b9b4258f6c603d17317b073181b71364675b
Steven Dana <stevendana86@gmail.com>
2020-08-16 20:16:53 -0400

    improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,85 +1,31 @@
 import React from "react"
-import WorkListItem from '../components/WorkListItem'
 import Masthead from '../components/Masthead'
 import SEO from "../components/seo"
-
-const work = [
-    {
-        slug: 'transcend',
-        thumbnail: '/images/logos/transcend-logo.svg',
-        categories: ['Logo Design', 'Branding'],
-        title: 'Transcend',
-    },
-    {
-        slug: 'knowledge-exchange',
-        thumbnail: '/images/knowledge-exchange/feed-in-laptop.png',
-        categories: ['Architecture', 'Design'],
-        title: 'Humana:',
-        subtitle: 'Knowledge Exchange'
-    },
-    {
-        slug: 'two-bards',
-        thumbnail: '/images/two-bards/two-bards-seal--purple.svg',
-        categories: ['Logo Design', 'Branding'],
-        title: 'Two Bards',
-    },
-    {
-        slug: 'ooh-icon-system',
-        video: '/images/ooh-icon-system/output.export.mp4',
-        categories: ['Development', 'Animation'],
-        title: 'Oohology',
-        subtitle: 'Icon System'
-    },
-    {
-        slug: 'champion-windows',
-        thumbnail: '/images/champion-windows/slider.png',
-        categories: ['Design'],
-        title: 'Champion',
-        subtitle: 'Windows'
-    }
-]
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
     <React.Fragment>
-        <SEO title="404: Not found" />
+        <SEO title="Page not found" />
         <section className="section section--hero section--index">
-            <div className="section__indicator section__indicator--x-margin-bottom">
-                01
-            </div>
             <Masthead
-                pill={
-                    <React.Fragment>
-                        <div>ポートフォリオ</div>
-                        <div className="masthead__timespan">'18–'19</div>
-                    </React.Fragment>
-                }
                 heading={
                     <React.Fragment>
-                        St<em>e</em>v<em>e</em>n D<em>a</em>n<em>a</em>
+                       <span className="hover-cursor">Steven Dana</span>
                     </React.Fragment>
                 }
                 text={
                     <React.Fragment>
-                        <div className="masthead__textline masthead__textline--large">Design & Development</div>
-                        <div className="masthead__textline masthead__textline--medium">Selected Work</div>
+                        <div className="masthead__textline masthead__textline--large -fade-in">Selected Projects</div>
                     </React.Fragment>
                 }
                 hasBackButton={false}
             />
         </section>
         <section className="section">
-            <div className="section__indicator">
-                02
+            <div className="deck">
+                <h2 className="deck__heading">Page not found</h2>
+                <p className="deck__text">Go back to <Link to="/">/</Link></p>
             </div>
-            <ul className="work">
-                {work.map((item, i) =>
-                    <WorkListItem
-                        {...item}
-                        upNext={work[i > 0 ? i - 1 : work.length - 1]}
-                        key={i}
-                    />
-                )}
-            </ul>
         </section>
     </React.Fragment>
 )
